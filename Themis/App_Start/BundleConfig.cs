e4a7b26d76500572e09c80d3b2b6a3edbb351b99
Rafael Vissontai de Oliveira <rafael.vissontai@outlook.com>
2017-07-11 18:29:43 -0300

    Implementação do Layout Admin, icones, fonts

--- Themis/App_Start/BundleConfig.cs
+++ Themis/App_Start/BundleConfig.cs
@@ -23,9 +23,20 @@ namespace Themis
                       "~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js"));
 
+            bundles.Add(new ScriptBundle("~/admin-lte/js").Include(
+                "~/admin-lte/js/app.js",
+                "~/admin-lte/plugins/fastclick/fastclick.js"
+             ));
+
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                      "~/Content/font-awesome.css",
+                      "~/Content/ionicons.css",
+                      "~/admin-lte/css/AdminLTE.css",
+                      "~/admin-lte/css/skins/skin-blue.css"));
+
+
         }
     }
 }
